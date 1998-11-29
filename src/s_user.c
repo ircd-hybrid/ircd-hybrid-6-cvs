e7ca11c81f0de7583ed49918ccbc303489445e87
db <db>
1998-11-29 07:04:22 +0000

    fixed bug in ANTI_SPAMBOT_EXTRA

--- src/s_user.c
+++ src/s_user.c
@@ -25,7 +25,7 @@
 static  char sccsid[] = "@(#)s_user.c	2.68 07 Nov 1993 (C) 1988 University of Oulu, \
 Computing Center and Jarkko Oikarinen";
 
-static char *rcs_version="$Id: s_user.c,v 1.25 1998/11/29 06:04:29 db Exp $";
+static char *rcs_version="$Id: s_user.c,v 1.26 1998/11/29 07:04:22 db Exp $";
 
 #endif
 
@@ -2865,12 +2865,12 @@ int	m_quit(aClient *cptr,
 	    }
 
 	  /* If its a client that has joined at least one channel
-	   * but not messaged anyone at all.. it might be trying to exit
-	   * with a spam message.
+	   * but not messaged anyone or a channel at all.. it might
+	   * be trying to exit with a spam message.
 	   */
 
 	  if(sptr->last_join_time &&
-	     !sptr->person_privmsgs && !sptr->channel_privmsgs)
+	     !(sptr->person_privmsgs | sptr->channel_privmsgs))
 	    {
 	      sendto_realops("Possible spambot exiting %s [%s@%s] [%s]",
 			     sptr->name, sptr->user->username,
