6da63b1e0bba0ffd32d2a1099987c45eed88f597
db <db>
1999-01-06 03:06:38 +0000

    Slow down whining of server full messages. once per 10 seconds

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -21,7 +21,7 @@
 #ifndef lint
 static  char sccsid[] = "@(#)s_bsd.c	2.78 2/7/94 (C) 1988 University of Oulu, \
 Computing Center and Jarkko Oikarinen";
-static char *rcs_version = "$Id: s_bsd.c,v 1.21 1999/01/05 02:49:01 chuegen Exp $";
+static char *rcs_version = "$Id: s_bsd.c,v 1.22 1999/01/06 03:06:38 db Exp $";
 #endif
 
 #include "struct.h"
@@ -1569,6 +1569,7 @@ void read_clients()
   int	res, length, fd;
   register int i;
   int rr;
+  time_t	last_full_to_opers_notice = (time_t)0;
 #ifdef USE_FAST_FD_ISSET
   int fd_mask;
   int fd_offset;
@@ -1906,8 +1907,13 @@ void read_clients()
 	    if (fd >= (HARD_FDLIMIT - 10))
 	      {
 		ircstp->is_ref++;
-		sendto_realops("All connections in use. (%s)",
-			       get_client_name(cptr, TRUE));
+		/* slow down the whining to opers bit */
+		if((last_full_to_opers_notice + 10) <= NOW)
+		  {
+		    sendto_realops("All connections in use. (%s)",
+				   get_client_name(cptr, TRUE));
+		    last_full_to_opers_notice = NOW;
+		  }
 		(void)send(fd,
 			   "ERROR :All connections in use\r\n",
 			   32, 0);
