04712d7fa26130b0560ac3364d4a13a946f3ab7c
tomh <tomh>
1999-07-03 08:18:22 +0000

    a bit more dependency cleanup

--- include/res.h
+++ include/res.h
@@ -1,7 +1,7 @@
 /*
  * irc2.7.2/ircd/res.h (C)opyright 1992 Darren Reed.
  *
- * $Id: res.h,v 1.4 1999/07/03 08:13:08 tomh Exp $
+ * $Id: res.h,v 1.5 1999/07/03 08:18:22 tomh Exp $
  */
 #ifndef	INCLUDED_res_h
 #define INCLUDED_res_h
@@ -11,9 +11,7 @@
 #define INCLUDED_sys_types_h
 #endif
 
-#ifndef INCLUDED_struct_h
-#include "struct.h"          /* aClient */
-#endif
+struct Client;
 
 struct DNSQuery {
   void* vptr;               /* pointer used by callback to identify request */
@@ -31,7 +29,7 @@ extern time_t	       timeout_query_list(time_t);
 extern time_t	       expire_cache(time_t);
 extern void            del_queries(const void* vptr);
 extern void            restart_resolver(void);
-extern unsigned long   cres_mem(aClient* cptr);
+extern unsigned long   cres_mem(struct Client* cptr);
 
 /*
  * add_local_domain - append local domain suffix to hostnames that 
