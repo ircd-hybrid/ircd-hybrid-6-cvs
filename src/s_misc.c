795c1f085d6d18b985c717f25ae680ef8d8e68b7
db <db>
1999-07-07 03:27:43 +0000

    - commited fixes for neph's stats p change to show O or o

--- src/s_misc.c
+++ src/s_misc.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: s_misc.c,v 1.38 1999/07/07 03:22:55 db Exp $
+ *  $Id: s_misc.c,v 1.39 1999/07/07 03:27:43 db Exp $
  */
 #include "s_conf.h"
 #include "struct.h"
@@ -1128,8 +1128,8 @@ void show_opers(aClient *cptr,char *name)
       j++;
       sendto_one(cptr, ":%s %d %s :%s [%c] (%s@%s) Idle: %d",
 		 me.name, RPL_STATSDEBUG, name,
-		 IsAnOper(cptr2)?'O':'o',
 		 cptr2->name,
+		 IsAnOper(cptr2)?'O':'o',
 		 cptr2->user->username, cptr2->user->host,
 		 timeofday - cptr2->user->last);
     }
