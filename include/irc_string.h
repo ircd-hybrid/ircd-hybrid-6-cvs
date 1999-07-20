a53535533467c45813fb7927880b346a2694bb89
tomh <tomh>
1999-07-20 00:56:36 +0000

    new char attrs

--- include/irc_string.h
+++ include/irc_string.h
@@ -16,7 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: irc_string.h,v 1.2 1999/07/19 09:05:10 tomh Exp $
+ *   $Id: irc_string.h,v 1.3 1999/07/20 00:56:36 tomh Exp $
  */
 #ifndef INCLUDED_irc_string_h
 #define INCLUDED_irc_string_h
@@ -78,30 +78,34 @@ extern void* MyRealloc(void* p, size_t size);
 /*
  * character macros
  */
-extern unsigned char ToLowerTab[];
+extern const unsigned char ToLowerTab[];
 #define ToLower(c) (ToLowerTab[(unsigned char)(c)])
 
-extern unsigned char ToUpperTab[];
+extern const unsigned char ToUpperTab[];
 #define ToUpper(c) (ToUpperTab[(unsigned char)(c)])
 
-extern unsigned int CharAttrs[];
+extern const unsigned int CharAttrs[];
 
-#define PRINT_C 0x01
-#define CNTRL_C 0x02
-#define ALPHA_C 0x04
-#define PUNCT_C 0x08
-#define DIGIT_C 0x10
-#define SPACE_C 0x20
-#define NICK_C  0x40
-#define CHAN_C  0x80
+#define PRINT_C   0x001
+#define CNTRL_C   0x002
+#define ALPHA_C   0x004
+#define PUNCT_C   0x008
+#define DIGIT_C   0x010
+#define SPACE_C   0x020
+#define NICK_C    0x040
+#define CHAN_C    0x080
+#define KWILD_C   0x100
+#define CHANPFX_C 0x200
 
-#define	IsNickChar(c) (CharAttrs[(unsigned char)(c)]  & NICK_C)
-#define	IsCntrl(c) (CharAttrs[(unsigned char)(c)]  & CNTRL_C)
-#define IsAlpha(c) (CharAttrs[(unsigned char)(c)]  & ALPHA_C)
-#define IsSpace(c) (CharAttrs[(unsigned char)(c)]  & SPACE_C)
-#define IsLower(c) (IsAlpha((c)) && ((unsigned char)(c) > 0x5f))
-#define IsUpper(c) (IsAlpha((c)) && ((unsigned char)(c) < 0x60))
-#define IsDigit(c) (CharAttrs[(unsigned char)(c)] & DIGIT_C)
+#define IsChanPrefix(c) (CharAttrs[(unsigned char)(c)] & CHANPFX_C)
+#define IsKWildChar(c)  (CharAttrs[(unsigned char)(c)] & KWILD_C)
+#define	IsNickChar(c)   (CharAttrs[(unsigned char)(c)] & NICK_C)
+#define	IsCntrl(c)      (CharAttrs[(unsigned char)(c)] & CNTRL_C)
+#define IsAlpha(c)      (CharAttrs[(unsigned char)(c)] & ALPHA_C)
+#define IsSpace(c)      (CharAttrs[(unsigned char)(c)] & SPACE_C)
+#define IsLower(c)      (IsAlpha((c)) && ((unsigned char)(c) > 0x5f))
+#define IsUpper(c)      (IsAlpha((c)) && ((unsigned char)(c) < 0x60))
+#define IsDigit(c)      (CharAttrs[(unsigned char)(c)] & DIGIT_C)
 #define	IsXDigit(c) (IsDigit(c) || 'a' <= (c) && (c) <= 'f' || \
 	'A' <= (c) && (c) <= 'F')
 #define IsAlNum(c) (CharAttrs[(unsigned char)(c)] & (DIGIT_C | ALPHA_C))
