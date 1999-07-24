7b8d084eecc22054eaf8d6c75cdd7cc5b848f0d3
sean <sean>
1999-07-24 20:02:50 +0000

    added num_bed to structure to count total bans/exceptions/denies

--- include/channel.h
+++ include/channel.h
@@ -62,6 +62,7 @@ struct Channel
   struct SLink*   banlist;
   struct SLink*   exceptlist;
   struct SLink*   denylist;
+  int             num_bed;  /* number of bans+exceptions+denies */
   time_t          channelts;
   int             locally_created;  /* used to flag a locally created channel */
   int             keep_their_modes; /* used only on mode after sjoin */
