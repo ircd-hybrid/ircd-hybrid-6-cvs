a04e0fe4a0bc3bde85ea497585181d4e2938fd30
lusky <lusky>
2000-10-12 03:09:58 +0000

    fix for *@*.foo.com I:lines being broken by a user@host.foo.com I:lines

--- src/mtrie_conf.c
+++ src/mtrie_conf.c
@@ -43,7 +43,7 @@
  *
  * Diane Bruce -db (db@db.net)
  *
- * $Id: mtrie_conf.c,v 1.69 2000/08/22 03:34:41 lusky Exp $
+ * $Id: mtrie_conf.c,v 1.70 2000/10/12 03:09:58 lusky Exp $
  */
 #include "mtrie_conf.h"
 #include "class.h"
@@ -839,6 +839,7 @@ static aConfItem *find_sub_mtrie(DOMAIN_LEVEL *cur_level,
   DOMAIN_PIECE *cur_piece;
   char *cur_dns_piece;
   aConfItem *aconf=(aConfItem *)NULL;
+  aConfItem *aconf_user=(aConfItem *)NULL;
 
   cur_dns_piece = dns_stack[--stack_pointer];
 
@@ -883,7 +884,8 @@ static aConfItem *find_sub_mtrie(DOMAIN_LEVEL *cur_level,
 
   if(stack_pointer == 0)
     {
-      return(find_user_piece(cur_piece,flags,cur_dns_piece,user));
+      aconf_user=find_user_piece(cur_piece,flags,cur_dns_piece,user);
+      return(aconf_user ? aconf_user : last_found_iline_aconf);
     }
 
   if(cur_piece->next_level)
