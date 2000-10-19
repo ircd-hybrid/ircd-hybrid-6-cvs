b7725a64b82ad4cb6e2806b5c0939b116def0488
lusky <lusky>
2000-10-19 04:18:53 +0000

    dns cache entries were never fully expiring, stayed in the hashtable forever

--- src/res.c
+++ src/res.c
@@ -4,7 +4,7 @@
  * shape or form. The author takes no responsibility for any damage or loss
  * of property which results from the use of this software.
  *
- * $Id: res.c,v 1.51 2000/06/10 02:45:37 lusky Exp $
+ * $Id: res.c,v 1.52 2000/10/19 04:18:53 lusky Exp $
  *
  * July 1999 - Rewrote a bunch of stuff here. Change hostent builder code,
  *     added callbacks and reference counting of returned hostents.
@@ -73,7 +73,7 @@
 #define NAMES_OFFSET  (size_t) (ADDRS_OFFSET + ADDRS_DLEN)
 #define MAXGETHOSTLEN (size_t) (NAMES_OFFSET + MAXPACKET)
 
-#define AR_TTL          600   /* TTL in seconds for dns cache entries */
+#define AR_TTL          600   /* minimum TTL in seconds for dns cache entries */
 
 /*
  * the following values should be prime
@@ -1629,7 +1629,7 @@ static void rem_cache(aCache* ocp)
    * remove cache entry from hashed name lists
    */
   assert(0 != hp->h_name);
-  if (hp->h_name)
+  if (hp->h_name == 0)
     return;
   hashv = hash_name(hp->h_name);
 
