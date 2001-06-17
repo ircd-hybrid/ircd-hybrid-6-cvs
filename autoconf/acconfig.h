ea1a27ab379f234a8ce4e2eab60cf6b8559fa06b
kreator <kreator>
2001-06-17 17:27:29 +0000

    - fix those mmap() tests and use ones that autoconf provides

--- autoconf/acconfig.h
+++ autoconf/acconfig.h
@@ -1,5 +1,5 @@
 /*
- * $Id: acconfig.h,v 1.3 1999/07/22 05:41:13 tomh Exp $
+ * $Id: acconfig.h,v 1.4 2001/06/17 17:27:34 kreator Exp $
  */
 
 /* Define only one of POSIX, BSD, or SYSV signals.  */
@@ -29,9 +29,6 @@
 
 /* Define if you have SYSV non-blocking sockets (FIONBIO) */
 #undef NBLOCK_SYSV
-
-/* Define if you have the mmap() function.  */
-#undef USE_MMAP
 
 /* Define if you are running DYNIXPTX.  */
 #undef OS_DYNIXPTX
