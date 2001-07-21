2f7400632ec8858f7b4902bb74b25e27279bc05b
jdc <jdc>
2001-07-21 17:05:16 +0000

    - src/s_bsd.c: connect_server(): "Host x.x.x.x is not enabled" message   changed to use aconf->name not aconf->host; "Host" also changed to   "Server".

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: s_bsd.c,v 1.139 2001/07/18 01:37:16 lusky Exp $
+ *  $Id: s_bsd.c,v 1.140 2001/07/21 17:05:16 jdc Exp $
  */
 #include "s_bsd.h"
 #include "class.h"
@@ -592,8 +592,8 @@ int connect_server(struct ConfItem* aconf,
    */
   if (!attach_cn_lines(cptr, aconf->name, aconf->host))
     {
-      sendto_realops("Host %s is not enabled for connecting:no C/N-line",
-                 aconf->host);
+      sendto_realops("Server %s is not enabled for connecting: no C/N-line",
+                 aconf->name);
       if (by && IsPerson(by) && !MyClient(by))
         sendto_one(by, ":%s NOTICE %s :Connect to host %s failed.",
                    me.name, by->name, cptr);
