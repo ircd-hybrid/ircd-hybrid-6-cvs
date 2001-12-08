cbf79df0f65ff44c2ee2a491c7e44095f26a4a36
db <db>
2001-12-08 17:55:06 +0000

    - register is an obsolete keyword left over from the early recursive descent   compilers days of unix. Good grief. Any modern compiler will generate better   code without this keyword getting in the way.   DIE DIE DIE !

--- src/sprintf_irc.c
+++ src/sprintf_irc.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: sprintf_irc.c,v 1.10 2001/12/08 08:34:50 jdc Exp $
+ *   $Id: sprintf_irc.c,v 1.11 2001/12/08 17:55:06 db Exp $
  */
 #include "sprintf_irc.h"
 
@@ -260,12 +260,11 @@ static char scratch_buffer[32];
  */
 
 int
-vsprintf_irc(register char *str, register const char *format,
-             register va_list args)
+vsprintf_irc(char *str, const char *format, va_list args)
 
 {
-        register char c;
-        register int bytes = 0;
+        char c;
+        int bytes = 0;
 
         while ((c = *format++))
         {
@@ -275,7 +274,7 @@ vsprintf_irc(register char *str, register const char *format,
 
                         if (c == 's')
                         {
-                                register const char *p1 = va_arg(args, const char *);
+                                const char *p1 = va_arg(args, const char *);
                                 if ((*str = *p1))
                                 {
                                         ++bytes;
@@ -301,8 +300,8 @@ vsprintf_irc(register char *str, register const char *format,
                          */
                         if (c == 'l' && *format == 'u')
                         {
-                                register unsigned long v1, v2;
-                                register const char *ap;
+                                unsigned long v1, v2;
+                                const char *ap;
 
                                 ++format;
                                 v1 = va_arg(args, unsigned long);
@@ -337,7 +336,7 @@ vsprintf_irc(register char *str, register const char *format,
                         }
                         if (c == 't')
                           {
-                            register unsigned int v1;
+                            unsigned int v1;
 
                             v1 = va_arg(args,int);
                             
@@ -351,9 +350,9 @@ vsprintf_irc(register char *str, register const char *format,
 
                         if (c == 'd')
                         {
-                                register unsigned int v1, v2;
-                                register const char *ap;
-                                register char *s = &scratch_buffer[sizeof(scratch_buffer) - 2];
+                                unsigned int v1, v2;
+                                const char *ap;
+                                char *s = &scratch_buffer[sizeof(scratch_buffer) - 2];
 
                                 v1 = va_arg(args, int);
                                 if ((int)v1 <= 0)
@@ -392,9 +391,9 @@ vsprintf_irc(register char *str, register const char *format,
 
                         if (c == 'u')
                         {
-                                register unsigned int v1, v2;
-                                register const char *ap;
-                                register char *s = &scratch_buffer[sizeof(scratch_buffer) - 2];
+                                unsigned int v1, v2;
+                                const char *ap;
+                                char *s = &scratch_buffer[sizeof(scratch_buffer) - 2];
 
                                 v1 = va_arg(args, unsigned int);
                                 if (v1 == 0)
@@ -427,7 +426,7 @@ vsprintf_irc(register char *str, register const char *format,
 
                         if (c != '%')
                         {
-                                register int ret;
+                                int ret;
 
                                 format -= 2;
                                 ret = vsprintf(str, format, args);
@@ -448,16 +447,10 @@ vsprintf_irc(register char *str, register const char *format,
 } /* vsprintf_irc() */
 
 int
-ircsprintf(register char *str, const char *format, ...)
+ircsprintf(char *str, const char *format, ...)
 {
-        /*
-         * commented out due to weird warning on rh7.1/gcc 2.96
-         */
-        /*
-        register va_list args;
-        */
         va_list args;
-        register int bytes;
+        int bytes;
 
         va_start(args, format);
 
