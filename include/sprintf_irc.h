e3c6155aa75938ff0d76b590a2f7c2b01661f078
db <db>
2001-12-08 17:56:52 +0000

    - register is OBSOLETE repeat after me   grr forgot to fix the prototype

--- include/sprintf_irc.h
+++ include/sprintf_irc.h
@@ -1,5 +1,5 @@
 /*
- * $Id: sprintf_irc.h,v 1.2 2000/10/06 03:00:57 lusky Exp $ 
+ * $Id: sprintf_irc.h,v 1.3 2001/12/08 17:56:52 db Exp $ 
  */
 
 #ifndef SPRINTF_IRC
@@ -11,8 +11,7 @@
  * Proto types
  */
 
-extern int vsprintf_irc(register char *str, register const char *format,
-    register va_list);
-extern int ircsprintf(register char *str, register const char *format, ...);
+extern int vsprintf_irc(char *str, const char *format, va_list);
+extern int ircsprintf(char *str, const char *format, ...);
 
 #endif /* SPRINTF_IRC */
