815bfaa07914c1292e9f6fad0adb00f534e8a479
lusky <lusky>
2001-12-12 02:26:49 +0000

    - revved patchlevel to 6.3rc3

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -15,9 +15,9 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * $Id: patchlevel.h,v 1.146 2001/12/11 02:20:43 lusky Exp $
+ * $Id: patchlevel.h,v 1.147 2001/12/12 02:26:49 lusky Exp $
  */
 
 #ifndef PATCHLEVEL
-#define PATCHLEVEL    "2.8/hybrid-6.3rc2"
+#define PATCHLEVEL    "2.8/hybrid-6.3rc3"
 #endif
