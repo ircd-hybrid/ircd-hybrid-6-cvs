f2f159bb3868d0288f4020cf20df929afc641471
androsyn <androsyn>
2002-01-13 23:31:27 +0000

    Error checking is your friend.  We weren't checking to see if mmap() fails at all.  Doh..

--- src/blalloc.c
+++ src/blalloc.c
@@ -4,17 +4,17 @@
  * Owner:  Wohali (Joan Touzet)
  *
  * Modified 2001/11/29 for mmap() support by Aaron Sethman <androsyn@ratbox.org>
- * $Id: blalloc.c,v 1.23 2002/01/13 22:32:45 androsyn Exp $
+ * $Id: blalloc.c,v 1.24 2002/01/13 23:31:27 androsyn Exp $
  */
 #include "config.h"
 #include "blalloc.h"
 #include "ircd_defs.h"      /* DEBUG_BLOCK_ALLOCATOR */
 #include "irc_string.h"     /* MyMalloc */
 #include "s_log.h"
-
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <assert.h>
 #ifdef HAVE_MMAP
 #include <sys/mman.h>
 #endif
@@ -30,7 +30,6 @@ static void *get_block(size_t size);
 
 extern void outofmemory(void);      /* defined in list.c */
 
-
 #ifdef HAVE_MMAP
 #ifndef MAP_ANON
 int zero_fd = -1;
@@ -44,7 +43,13 @@ void initBlockHeap(void)
 }
 static void *get_block(size_t size)
 {
-    return (mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, zero_fd, 0));
+    void *ptr;
+    assert(zero_fd >= 0);
+    assert(size > 0);
+    ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, zero_fd, 0);
+    if(ptr == MAP_FAILED)
+    	ptr = NULL;
+    return(ptr); 
 }
 
 #else /* MAP_ANON */
@@ -55,8 +60,13 @@ void initBlockHeap(void)
 }
 static void *get_block(size_t size)
 {
-    return (mmap(NULL, size, PROT_READ | PROT_WRITE,
-                 MAP_PRIVATE | MAP_ANON, -1, 0) );
+    void *ptr; 
+    assert(size > 0);
+    ptr = mmap(NULL, size, PROT_READ | PROT_WRITE,
+              	MAP_PRIVATE | MAP_ANON, -1, 0);
+    if(ptr == MAP_FAILED)
+    	ptr = NULL;
+    return(ptr);
 }
 #endif /* MAP_ANON */
 
