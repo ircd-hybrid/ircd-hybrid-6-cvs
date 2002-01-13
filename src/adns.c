e067b54910e05401b0d434277bfa60cceaaa4bc6
androsyn <androsyn>
2002-01-13 04:10:42 +0000

    Fix the bug I just fixed in -7.

--- src/adns.c
+++ src/adns.c
@@ -1,5 +1,5 @@
 /*
- * $Id: adns.c,v 1.5 2001/12/09 18:16:56 lusky Exp $
+ * $Id: adns.c,v 1.6 2002/01/13 04:10:42 androsyn Exp $
  * adns.c  functions to enter libadns 
  *
  * Written by Aaron Sethman <androsyn@ratbox.org>
@@ -49,12 +49,18 @@ static void dns_cancel_all(void)
  adns_forallqueries_begin(dns_state);
  while((q = adns_forallqueries_next(dns_state, (void **)&r)) != NULL)
  {
-	adns_cancel(q);
-	adns__query_done(q);
- 	adns_check(dns_state, &q, &answer, (void **)&query);
- 	assert(query->callback != NULL);
-	query->query = NULL;
- 	query->callback(query->ptr, NULL);
+	if(q->state != query_done)
+	{
+		adns_cancel(q);
+		adns__query_done(q);
+ 		adns_check(dns_state, &q, &answer, (void **)&query);
+ 	}
+ 	if(query->callback != NULL)
+ 	{
+		MyFree(query->query);
+		query->query = NULL;
+ 		query->callback(query->ptr, NULL);
+	}
  }
 }
 
