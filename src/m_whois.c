0a50a7b10f809678ea8ebfc4d5939192479cc9bf
ievil <ievil>
2003-10-13 11:21:40 +0000

    Enhancements to whois notifications...

--- src/m_whois.c
+++ src/m_whois.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_whois.c,v 1.15 2003/10/13 10:01:33 ievil Exp $
+ *   $Id: m_whois.c,v 1.16 2003/10/13 11:21:40 ievil Exp $
  */
 
 #include "m_operspylog.h"
@@ -323,10 +323,23 @@ int     m_whois(struct Client *cptr,
 #endif
              (acptr != sptr))
               
+#ifdef SHOW_REMOTE_WHOIS
+          if (MyConnect(sptr))              
+#endif /* SHOW_REMOTE_WHOIS */
             sendto_one(acptr,
                        ":%s NOTICE %s :*** Notice -- %s (%s@%s) is doing a /whois on you.",
                        me.name, acptr->name, parv[0], sptr->username,
                        sptr->host);
+#ifdef SHOW_REMOTE_WHOIS
+          else
+            sendto_one(acptr,
+                       ":%s NOTICE %s :*** Notice -- %s (%s@%s) is doing a /whois on you. [%s]",
+                       me.name, acptr->name, parv[0], sptr->username,
+                       sptr->host, sptr->user->server);
+
+#endif /* SHOW_REMOTE_WHOIS */
+
+
 #endif /* #ifdef WHOIS_NOTICE */
 
 
@@ -489,11 +502,22 @@ int     m_whois(struct Client *cptr,
               (MyConnect(sptr)) && (IsPerson(sptr)) &&
 #endif
              (acptr != sptr))
-
+#ifdef SHOW_REMOTE_WHOIS
+          if (MyConnect(sptr))
+#endif /* SHOW_REMOTE_WHOIS */
             sendto_one(acptr,
                        ":%s NOTICE %s :*** Notice -- %s (%s@%s) is doing a /whois on you.",
                        me.name, acptr->name, parv[0], sptr->username,
                        sptr->host);
+#ifdef SHOW_REMOTE_WHOIS
+          else
+            sendto_one(acptr,
+                       ":%s NOTICE %s :*** Notice -- %s (%s@%s) is doing a /whois on you. [%s]",
+                       me.name, acptr->name, parv[0], sptr->username,
+                       sptr->host, sptr->user->server);
+
+#endif /* SHOW_REMOTE_WHOIS */
+
 #endif /* #ifdef WHOIS_NOTICE */
 
           if ((acptr->user
